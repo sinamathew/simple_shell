89b199914f6896547408fa061756b0442cb16fbc
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-09-09 20:28:08 +0100

    Added proper betty coding style and documentation

--- shell/shell.h
+++ shell/shell.h
@@ -1,17 +1,23 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+
+/*---LIBRARIES---*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
+
+/*---PREPROCESSORs---*/
 #define MAX_INPUT_SIZE 1024
 
-void display_prompt();
+
+/*---PROTOTYPES---*/
+void display_prompt(void);
 int read_input(char *input, size_t size);
 int execute_command(const char *input);
 
-#endif /* SHELL_H */
 
+#endif /* SHELL_H */
