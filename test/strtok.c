fa820125162c0cac27cca45abb0f2c3db097996d
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-09-11 18:42:11 +0100

    Added String Token

--- test/strtok.c
+++ test/strtok.c
@@ -2,4 +2,22 @@
 
 int main(void)
 {
+	char *str = NULL;
+	size_t size = 0;
+	char *token;
+	int i;
+
+	while (getline(&str, &size, stdin) != -1)
+	{
+		token = strtok(str, " \n");
+		for (i = 0; token != NULL; i++)
+		{
+			printf("Number %d string is '%s'\n", i + 1, token);
+			token = strtok(NULL, " \n");
+		}
+	}
+
+	free(str);
+	fprintf(stderr, "Error\n");
+	return (1);
 }
