cfedc954de122d12b22f86a94bcc94d11c770711
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-09-20 22:12:34 +0100

    update utility file

--- utils.c
+++ utils.c
@@ -7,53 +7,88 @@
  */
 void display_prompt(void)
 {
-	printf("$ ");
-	fflush(stdout);
+	char prompt[] = "$ ";
+
+	write(STDOUT_FILENO, prompt, sizeof(prompt) - 1);
 }
 
 
 /**
  * read_input - reads inputs from stdin
- * By: Noble
- * @input: pointer to store the input
- * @size: allocated memory for input
+ * By: Noble && Sina
+ * @input: pointer to a pointer to the input
+ * @size: pointer to allocated memory for input
  * Return: -1 if fail, and 0 on success
  */
-int read_input(char *input, size_t size)
+int read_input(char **input, size_t *size)
 {
-	if (fgets(input, size, stdin) == NULL)
+	char *line = NULL;
+	ssize_t n;
+	*size = 0;
+
+	do {
+		n = read(STDIN_FILENO, &line[*size], 1);
+
+		if (n == -1)
+		{
+			free(line);
+			return (-1);
+		}
+
+		(*size)++;
+	} while (n > 0 && line[*size - 1] != '\n');
+
+	if (*size == 1 && line[0] == '\n')
 	{
+		free(line);
 		return (-1);
 	}
+
+	line[*size - 1] = '\0';
+	*input = line;
+
 	return (0);
 }
 
 
 /**
- * execute_command - don't know yet
- * @input: don't know yet
- * Return: -1 or 0
- * By: Noble
+ * execute_command - execute command taking from input
+ * @input: pointer to the command
+ * Return: 0 on success and -1 on error
+ * By: Noble && Sina
  */
-int execute_command(const char *input)
+int execute_command(char *input)
 {
 	pid_t pid = fork();
 	int status;
+	char not_found[] = "Command not found: ";
+	char new_line[] = "\n";
+	char *args[2];
+	char error_msg[] = "Error: execve\n";
+	args[0] = input;
+	args[1] = NULL;
 
 	if (pid == -1)
-		return (-1);
-
-	else if (pid == 0)
 	{
-		if (execlp(input, input, NULL) == -1)
-			return (-1);
+		write(STDERR_FILENO, "Error: fork\n", 12);
+		return (-1);
+	}
+
+	if (pid == 0 && execve(input, args, NULL) == -1)
+	{
+		write(STDERR_FILENO, error_msg, sizeof(error_msg) - 1);
+		_exit(EXIT_FAILURE);
 	}
 
 	else
 	{
 		waitpid(pid, &status, 0);
-		if (status != 0)
-			fprintf(stderr, "Command not found: %s\n", input);
+		if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+		{
+			write(STDERR_FILENO, not_found, sizeof(not_found) - 1);
+			write(STDERR_FILENO, input, sizeof(input) - 1);
+			write(STDERR_FILENO, new_line, sizeof(new_line) - 1);
+		}
 	}
 	return (0);
 }
