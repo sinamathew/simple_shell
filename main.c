9afd6e491f5295aced026971c632dd236e5dac6c
Ahorlu Noble <ahorlunoble7@gmail.com>
2023-09-22 15:34:54 -0700

    Doing hard things

--- main.c
+++ main.c
@@ -1,44 +1,30 @@
 #include "shell.h"
 
 /**
- * main - entry point
- * @ac: arg count
- * @av: arg vector
- *
- * Return: 0 on success, 1 on error
+ * main - Simple Shell main function
+ * @ac: Count of arguments
+ * @av: Arguments
+ * Return: 0 Always (success).
  */
-int main(int ac, char **av)
+int main(int ac, char **argv)
 {
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
+	char *line = NULL;
+	/*char **command = NULL;*/
+	int status = 0;
+	(void) ac;
+	(void) argv;
 
-	asm ("mov %1, %0\n\t"
-			"add $3, %0"
-			: "=r" (fd)
-			: "r" (fd));
-
-	if (ac == 2)
+	while (1)
 	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
+		line = read_line();
+		if (line == NULL)
 		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			return (status);
 		}
-		info->readfd = fd;
+		free(line);
+		/*command = tokennizer(line);*/
+		/*status = _execute(command, argv);*/
 	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
-	return (EXIT_SUCCESS);
 }
